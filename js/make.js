678e964ed475be7e4ff71c8ddfe77d5ad12a720f
Elvis Tony <elvistonyrency@gmail.com>
2020-09-26 16:12:06 +0530

    Revamped the Counter

---
+++ js/make.js
@@ -0,0 +1,75 @@
+var mode="duration"
+    function changeMode(ele){
+      mode = ele.value;
+        document.getElementById('duration').classList.toggle('w3-hide');
+        document.getElementById('time').classList.toggle('w3-hide');
+    }    
+
+
+
+    var d = new Date();
+    document.getElementsByName('time_H')[0].innerText=d.getHours()+""
+    function SaveSettings(){
+      document.getElementById('save').innerText="Saved !";
+      document.getElementById('save').classList.remove('w3-orange');
+      document.getElementById('save').classList.add('w3-green');
+      document.getElementById('save').classList.add('w3-hover-green');
+      setTimeout(()=>{
+        document.getElementById('save').innerText="Save Settings";
+      document.getElementById('save').classList.remove('w3-green');
+      document.getElementById('save').classList.add('w3-orange');
+      document.getElementById('save').classList.remove('w3-hover-green');
+      },2000)
+      let time;
+      if(mode=="duration"){
+        time = document.getElementsByName('dur_H')[0].value +":"+document.getElementsByName('dur_M')[0].value +":"+document.getElementsByName('dur_S')[0].value;
+      }else{
+        time = document.getElementsByName('time_H')[0].value +":"+document.getElementsByName('time_M')[0].value +":"+document.getElementsByName('time_S')[0].value;
+      }
+      var jsn = {
+        "title":document.getElementsByName('title')[0].value,
+        "posttimer":document.getElementsByName('posttimer')[0].value,
+        "youtubeID":youtube_parser(document.getElementsByName('youtubeID')[0].value),
+        "mode":mode,
+        "blur":document.getElementsByName('blur')[0].checked==true?true:false,
+        "time":time,
+        "msgs":document.getElementsByName('msgs')[0].value
+      }
+      console.log(JSON.stringify(jsn));
+      setCookie(JSON.stringify(jsn))
+    }
+
+    function youtube_parser(url){
+        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+        var match = url.match(regExp);
+        return (match&&match[7].length==11)? match[7] : false;
+    }
+
+    function setOptions(){
+      var data = getCookie('Counter')
+      if(data==""){
+        setCookie('{"title":"The Event will begin in","posttimer":"a matter of moments","youtube_id":"hYgKv4ZxaCM","mode":"duration","blur":true,"time":"17:10"}')
+        setOptions()
+      }else{
+          console.log(data);
+        var parsed = JSON.parse(data)
+        document.getElementsByName('title')[0].value = parsed["title"]
+        document.getElementsByName('posttimer')[0].value = parsed["posttimer"]
+        document.getElementsByName('youtubeID')[0].value ="https://www.youtube.com/watch?v="+parsed["youtubeID"]
+        if(parsed["blur"]==true){
+          document.getElementsByName('blur')[0].checked=true;
+        }
+        if(parsed['mode']=="duration"){
+          document.getElementsByName('dur_H')[0].value = parsed['time'].split(':')[0]
+          document.getElementsByName('dur_M')[0].value = parsed['time'].split(':')[1]
+          document.getElementsByName('dur_S')[0].value = parsed['time'].split(':')[2]
+        }else{
+          document.getElementsByName('time_H')[0].value = parsed['time'].split(':')[0]
+          document.getElementsByName('time_M')[0].value = parsed['time'].split(':')[1]
+          document.getElementsByName('time_S')[0].value = parsed['time'].split(':')[2]  
+        }
+      }
+      document.getElementsByName('msgs')[0].value=parsed['msgs'];
+    }
+
+    setOptions()
---
+++ js/yttimer.js
@@ -0,0 +1,143 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let destination;
+
+function onPlayerReady(event) {
+  var playButton = document.getElementById("play-button");
+  console.log(player.getDuration());
+  CountDown(destination,player.getDuration())
+}
+
+var player;
+var vidElement = document.getElementById('ifr')
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ifr', {
+    events: {
+        'onReady': onPlayerReady
+    },
+    videoId: 'bpOR_HuHRNs',
+    });
+}
+
+var videoOn=false;
+function startVideoAt(seeker) {
+  if(videoOn==false){
+    player.seekTo(seeker)
+    player.playVideo()
+    player.setVolume(70);
+    videoOn=true;
+  }
+}
+
+var toast = document.getElementsByClassName('toast')[0];
+
+function toaster(msg){
+  toast.innerHTML=msg;
+  toast.style.opacity="0.6";
+  toast.style.bottom="5%";
+  toast.style.fontSize="20px";
+  setTimeout(function(){
+    toast.style.opacity="0";
+    toast.style.bottom="8%";
+    toast.style.fontSize="18px";
+  },6000)
+  setTimeout(function(){
+    toast.style.bottom="2%"
+  },7000)
+}
+
+var msgs=[]
+
+  let postmsg;
+  function setOptions(){
+  var data = getCookie('Counter')
+  if(data==""){
+    setCookie('{"title":"The Event will begin in","posttimer":"a matter of moments","youtube_id":"hYgKv4ZxaCM","mode":"duration","blur":true,"time":"17:10"}')
+    setOptions()
+  }else{
+    var parsed = JSON.parse(data)
+    if(parsed['youtubeID']!=""){
+      document.getElementById('ifr').src='https://www.youtube-nocookie.com/embed/'+parsed['youtubeID']+'?enablejsapi=1&controls=0&rel=0'
+    }
+    document.getElementsByClassName("title")[0].innerText=parsed['title'];
+    if(parsed['blur']==true){
+      document.getElementById('ifr').style.filter='blur(6px)';
+    }
+    postmsg = parsed['posttimer'];
+    var d1 = new Date ();
+    var d = new Date (d1);
+    if(parsed['mode']=='duration'){
+      console.log(d);
+      d.setHours(d.getHours()+(parsed['time'].split(':')[0]*1))
+      d.setMinutes(d.getMinutes()+(parsed['time'].split(':')[1]*1))
+      d.setSeconds(d.getSeconds()+(parsed['time'].split(':')[2]*1))
+      console.log(d);
+    }else{
+      d.setHours(d.getHours())
+      d.setMinutes(parsed['time'].split(':')[1]*1)
+      d.setSeconds(parsed['time'].split(':')[2]*1)
+    }
+    destination=d.getTime();
+    var m = decodeURI(parsed['msgs']).split('^')
+    if(m.length>0){
+        msgs=m;
+    }
+  }
+}
+
+setOptions()
+var toasting = false;
+
+function StartToasts(){
+  if (toasting==true){
+    return;
+  }
+  i=0;
+  toasting=true;
+  var tt = setInterval(function() {
+    if(i<msgs.length){
+      toaster(msgs[i])
+      i++
+    }else{
+      clearInterval(tt)
+      toasting=false;
+    }
+  },10000)
+}
+
+
+function CountDown(date,vidlen) {
+  var counter = document.getElementsByClassName('subtitle')[0]
+  var Time
+  var countDownDate = date
+  var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    counter.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+    onlySec = Math.floor((distance % (1000 * 60 * 60)) / (1000 ));
+
+    if(onlySec <= vidlen+2){
+      startVideoAt(vidlen-onlySec+2)
+    }
+    StartToasts()
+    if (distance<=0) {
+      clearInterval(x);
+      vidElement.style.opacity="0";
+      setTimeout(()=>{
+        //
+      },500)
+      counter.innerHTML = "a few Moments!";
+    }
+    if (minutes==0 && seconds == 4 ) {
+      document.getElementsByClassName('cover')[0].style.opacity=1;
+      
+      vidElement.style.opacity="0";
+    }
+  }, 1000);
+}
